e37d03e9ebd18999b9c74ff1d08de26ad06756dc
Andre Lucho <andretlucho@gmail.com>
2025-09-05 10:04:31 -0300

    Att até 04/09/25

---
+++ maisPraTI/01-Aulas/04-Java/oop/src/ProgramacaoOrientadaAObjetos.java
@@ -0,0 +1,53 @@
+// Características de um Objeto (concretos e abstratos)
+// ----------------------------------------------------
+
+// Concretos
+// ----------------------
+// Objetos do mundo real
+
+// Abstratos
+// ------------------------------------------------------------
+// Processo de compra em um e-commerce, login do usuário, um
+// componente do React
+
+// Regras de negócios e componentes de um projeto também podem ser representados
+// como objetos
+
+// Abstração - descreve as características:
+// ----------------------------------------
+
+// 1. O que ele é?
+// 2. Características? Como ele é?
+// 3. O que ele faz?
+// 4. Como ele está? Aspectos
+
+// Pilares da POO (4):
+// ---------------------
+
+// Abstração
+// -----------------------------------
+// Acima
+
+// Encapsulamento
+// -----------------------------------
+// Só tenho acesso a alguns métodos e propriedades - as demais funcionam por
+// trás ou funcionam p que os métodos e propriedades herdadas das classes-filhos
+// funcionarem
+
+// Herança
+// -----------------------------------
+// Capacidade de classes-filhos herdarem características de suas classes-pai,
+// como propriedades(atributos) e métodos
+
+// Polimorfismo
+// -----------------------------------
+// Mtas formas de escrever o mesmo método em diferentes classes, com base na
+// assinatura de um método ---> baseado em quantidade e tipo de parâmetro
+
+// Posso ter:
+// 1. Métodos com o mesmo nome em classes diferentes;
+// 2. Métodos com o mesmo nome com assinaturas diferentes dentro de uma mesma
+// classe
+
+// 1. Sobreescrita - nomes e classes diferentes
+// 2. Sobrecarga - mesma classe, mesmo método
