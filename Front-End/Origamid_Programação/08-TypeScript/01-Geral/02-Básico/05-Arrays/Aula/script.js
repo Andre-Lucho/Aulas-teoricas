64ff214605f362e674fbf9fd443f4230db996cf5
Andre-Lucho <andretlucho@gmail.com>
2026-01-15 14:05:53 -0300

    ATT até 15/01/26

---
+++ Front-End/Origamid_Programação/08-TypeScript/01-Geral/02-Básico/05-Arrays/Aula/script.js
@@ -0,0 +1,24 @@
+"use strict";
+const numeros = [10, 30, 40, 5, 3, 30];
+const valores = [10, 'Taxas', 40, 'Produto', 3, 30];
+function maiorQue10(data) {
+    return data.filter((n) => n > 10);
+}
+function filtrarValores(data) {
+    return data.filter((item) => typeof item === 'number');
+}
+//Sintaxe Alternativa
+function maiorQue20(data) {
+    return data.filter((n) => n > 20);
+}
+function filtrarStrings(data) {
+    return data.filter((item) => typeof item === 'string');
+}
+console.log(maiorQue10(numeros));
+console.log(maiorQue20(numeros));
+console.log(filtrarValores(valores));
+console.log(filtrarStrings(valores));
+const dados = [
+    ['senhor dos aneis', 80],
+    ['a guerra dos tronos', 120],
+];
