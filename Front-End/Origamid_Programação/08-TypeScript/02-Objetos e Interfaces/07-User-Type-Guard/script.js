cac0088a8cb9421215d2f14d654ac7091bc37683
Andre-Lucho <andretlucho@gmail.com>
2026-03-04 14:12:57 -0300

    Att até 03/03/26

--- Front-End/Origamid_Programação/08-TypeScript/02-Objetos e Interfaces/07-User-Type-Guard/script.js
+++ Front-End/Origamid_Programação/08-TypeScript/02-Objetos e Interfaces/07-User-Type-Guard/script.js
@@ -1,23 +1,32 @@
 "use strict";
-// async function fetchCursos() {
-//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
-//   const json = await response.json();
-//   handleCursos(json);
-// }
-// fetchCursos();
-// // 1)
-// function handleCursos(data: unknown) {
-//   // aqui, o TS verifica internamente se data é uma instância de Array
-//   if (data instanceof Array) {
-//     console.log('É instância de Array');
-//   }
-//   // aqui, o TS não executa JS (método do Array)... como ele sabe que é um array?
-//   if (Array.isArray(data)) {
-//     console.log('É array');
-//   }
-// }
-// 2)
-// function tipo user type guard:
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const json = await response.json();
+    // handleCursos(json);
+    handleCursos2(json);
+}
+fetchCursos();
+/*
+
+1)
+---------------------
+
+function handleCursos(data: unknown) {
+  // aqui, o TS verifica internamente se data é uma instância de Array
+  if (data instanceof Array) {
+    console.log('É instância de Array');
+  }
+  // aqui, o TS não executa JS (método do Array)... como ele sabe que é um array? --> usa Type Predicate
+  if (Array.isArray(data)) {
+    console.log('É array');
+  }
+}
+*/
+/*
+
+2)
+----------------------- */
+// function tipo User Type Guard:
 function isString(value) {
     return typeof value === 'string';
 }
@@ -36,6 +45,42 @@ function handleData2(data) {
         console.log(data.toUpperCase());
     }
 }
-handleData('Origamid');
-handleData(200);
-handleData2(200);
+// const handleCursos2 = (data: unknown) => {
+//   if (data instanceof Curso) {
+//     console.log(data.map((curso: Curso) => curso.nome));
+//   }
+// };
+// dá erro pois interface não é uma instância
+// 3.2)
+const isCurso = (value) => {
+    if (value && typeof value === 'object' && 'nome' in value) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+/*
+
+OBS: no if() acima:
+--------------------
+1. Verifica se 'value' é true --> não é null, undefined ... mas pode ser tipo primitivo, então verifico:
+2. 'typeof value === 'object'' --> verifica se o tipo de value é objeto
+3. 'nome' in value --> verifica se o objeto possui a propriedade 'nome' **
+** aqui o TS já está considerando o value como um objeto, então posso usar o 'in'
+
+
+com API's que retornam Arrays:
+-------------------------------
+if (Array.isArray(value) && value.length > 0 && 'nome' in value[0])
+
+1. verificando se temos uma array retornando da API;
+2. se a array não está vazia;
+3. se o primeiro item da array possui a propriedade 'nome'.
+
+*/
+const handleCursos2 = (data) => {
+    if (isCurso(data)) {
+        console.log(data.nome);
+    }
+};
--- Front-End/Origamid_Programação/08-TypeScript/02-Objetos e Interfaces/07-User-Type-Guard/script.ts
+++ Front-End/Origamid_Programação/08-TypeScript/02-Objetos e Interfaces/07-User-Type-Guard/script.ts
@@ -1,25 +1,34 @@
-// async function fetchCursos() {
-//   const response = await fetch('https://api.origamid.dev/json/cursos.json');
-//   const json = await response.json();
-//   handleCursos(json);
-// }
-// fetchCursos();
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const json = await response.json();
+  // handleCursos(json);
+  handleCursos2(json);
+}
+fetchCursos();
 
-// // 1)
-// function handleCursos(data: unknown) {
-//   // aqui, o TS verifica internamente se data é uma instância de Array
-//   if (data instanceof Array) {
-//     console.log('É instância de Array');
-//   }
-//   // aqui, o TS não executa JS (método do Array)... como ele sabe que é um array?
-//   if (Array.isArray(data)) {
-//     console.log('É array');
-//   }
-// }
+/*
 
-// 2)
+1)
+---------------------
 
-// function tipo user type guard:
+function handleCursos(data: unknown) {
+  // aqui, o TS verifica internamente se data é uma instância de Array
+  if (data instanceof Array) {
+    console.log('É instância de Array');
+  }
+  // aqui, o TS não executa JS (método do Array)... como ele sabe que é um array? --> usa Type Predicate
+  if (Array.isArray(data)) {
+    console.log('É array');
+  }
+}
+*/
+
+/*
+
+2)
+----------------------- */
+
+// function tipo User Type Guard:
 function isString(value: unknown): value is string {
   return typeof value === 'string';
 }
@@ -42,7 +51,64 @@ function handleData2(data: unknown) {
   }
 }
 
-handleData('Origamid');
-handleData(200);
+// handleData('Origamid');
+// handleData(200); // nem chega a executar e não dá erro --> não passa no Type Guard
 
-handleData2(200);
+// handleData2(200); //erro passou no type guard, mas o método não é de Number
+
+/*
+
+3 ) Objetos
+----------------------
+
+3.1) */
+interface Curso {
+  nome: string;
+  aulas: number;
+  nivel: string;
+  gratuito: boolean;
+}
+
+// const handleCursos2 = (data: unknown) => {
+//   if (data instanceof Curso) {
+//     console.log(data.map((curso: Curso) => curso.nome));
+//   }
+// };
+
+// dá erro pois interface não é uma instância
+
+// 3.2)
+
+const isCurso = (value: unknown): value is Curso => {
+  if (value && typeof value === 'object' && 'nome' in value) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/* 
+
+OBS: no if() acima:
+--------------------
+1. Verifica se 'value' é true --> não é null, undefined ... mas pode ser tipo primitivo, então verifico:
+2. 'typeof value === 'object'' --> verifica se o tipo de value é objeto
+3. 'nome' in value --> verifica se o objeto possui a propriedade 'nome' **
+** aqui o TS já está considerando o value como um objeto, então posso usar o 'in'
+
+
+com API's que retornam Arrays:
+-------------------------------
+if (Array.isArray(value) && value.length > 0 && 'nome' in value[0])
+
+1. verificando se temos uma array retornando da API;
+2. se a array não está vazia;
+3. se o primeiro item da array possui a propriedade 'nome'.
+
+*/
+
+const handleCursos2 = (data: unknown) => {
+  if (isCurso(data)) {
+    console.log(data.nome);
+  }
+};
