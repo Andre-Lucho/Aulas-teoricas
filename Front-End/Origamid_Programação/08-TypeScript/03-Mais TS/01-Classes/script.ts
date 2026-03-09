07f4d9f21cd54285463d0a264dc5dfeb10bf2665
Andre-Lucho <andretlucho@gmail.com>
2026-03-09 16:03:34 -0300

    Att até 09/03/26

--- Front-End/Origamid_Programação/08-TypeScript/03-Mais TS/01-Classes/script.ts
+++ Front-End/Origamid_Programação/08-TypeScript/03-Mais TS/01-Classes/script.ts
@@ -1,7 +1,8 @@
 class Produto {
+  //*
   tipo = 'produto';
   nome: string;
-  preco: number | undefined; //*
+  preco: number | undefined; //**
   constructor(nome: string, preco?: number) {
     this.nome = nome;
     this.preco = preco;
@@ -10,4 +11,74 @@ class Produto {
 
 const livro = new Produto('O Senhor dos Anéis');
 
-// * como preco é um parâmetro opcional, ele pode ser undefined
+/*
+
+1.*) TS - no corpo da classe, definimos o tipo de cada propriedade
+2. **) como preco é um parâmetro opcional(?.), ele pode ser undefined */
+
+// 2. Modificadores
+// --------------------
+
+class Produto2 {
+  private tipo = 'produto';
+  protected preco: number;
+  readonly nome: string;
+  constructor(nome: string, preco: number) {
+    this.nome = nome;
+    this.tipo;
+    this.preco = preco;
+  }
+  getTipo() {
+    return this.tipo;
+  }
+  getPreco() {
+    return Produto2.transformarPreco(this.preco);
+  }
+  // // [javascript] static: não fará parte do Objeto criado e sim da função construtora (classe)
+  static transformarPreco(preco: number) {
+    return `R$ ${preco}`;
+  }
+}
+
+const livro2 = new Produto2('O Senhor dos Aneis', 200);
+// console.log(livro2.getTipo());
+console.log(livro2.getPreco());
+console.log(livro2.nome);
+console.log(Produto2.transformarPreco(100)); // Está na classe Produto e nao na instância livro2
+
+livro2.nome = 'O Hobbit'; // readonly - não pode ser modificado
+livro2.tipo = 'livro'; // private
+
+console.log(livro2.nome);
+console.log(livro2.tipo);
+
+// 3. Class e Interface
+// ------------------------------
+
+class Quadrado {
+  readonly lados = 4;
+  medida: number;
+  constructor(medida: number) {
+    this.medida = medida;
+  }
+  getPerimetro() {
+    return this.medida * this.lados;
+  }
+  getArea() {
+    return this.medida * this.medida;
+  }
+}
+
+class Circulo {
+  readonly PI = Math.PI;
+  raio: number;
+  constructor(raio: number) {
+    this.raio = raio;
+  }
+  getPerimetro() {
+    return Math.round(2 * this.PI * this.raio * 100) / 100;
+  }
+  getArea() {
+    return Math.round(this.PI * (this.raio * this.raio) * 100) / 100;
+  }
+}
